885a15a0e0da5f0b385fc2372f2efc0a17548463
psfhoang <61898114+psfhoang@users.noreply.github.com>
2020-03-29 16:19:42 +0700

    Add files via upload

--- Phanso.java
+++ Phanso.java
@@ -16,26 +16,26 @@ public class Phanso {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        phanso  a=  new phanso(5,6);
-        phanso b =new phanso(1,6);
-        b.cong2ps(a);
-        b.toigianps();
-        
-        
-        
-        b.show();
-        
-        
+        phanso a = new phanso(5, 6);
+        phanso b = new phanso(1, 6);
+
+        //b.cong2ps(a);
+        //b.toigianps();
+        phanso c = nhan2phanso(a, b);
+        c.toigianps();
+        c.show();
+
     }
-     static class  phanso
-    {
+
+    static class phanso {
+
         private int tu;
         private int mau;
-        public  phanso(int tu, int mau)
-        {
-            
-            this.tu=tu;
-            this.mau=mau;
+
+        public phanso(int tu, int mau) {
+
+            this.tu = tu;
+            this.mau = mau;
         }
 
         public void setTu(int tu) {
@@ -53,62 +53,64 @@ public class Phanso {
         public int getMau() {
             return mau;
         }
-        public int UCLN(int a, int b)
-        {
-           
-            while(a%b!=0)
+
+        public int UCLN(int a, int b) {
+            int k;
+            if(a<b)
             {
-                b=a%b;
+                k=a;
                 a=b;
+                b=k;
+                
+            }
+                
+
+            while (a % b != 0) {
+                b = a % b;
+                a = b;
             }
             return b;
         }
-        public void cong2ps(phanso a)
-        {
-            this.tu=this.tu*a.mau+this.mau*a.tu;
-            this.mau=this.mau*a.mau;
-            
-            
-            
+
+        public void toigianps() {
+            int i = UCLN(this.tu, this.mau);
+            this.tu = this.tu / i;
+            this.mau = this.mau / i;
+
         }
-        public void tru2ps(phanso a)
-        {
-            this.tu=this.tu*a.mau-this.mau*a.tu;
-            this.mau=this.mau*a.mau;
-            
+
+        public void show() {
+            System.out.println(this.tu + "/" + this.mau);
         }
-        public void nhan2ps(phanso a)
-        {
-            this.tu=this.tu*a.tu;
-            this.mau=this.mau*a.mau;
-           
-            
-        }
-        public void chia2ps(phanso a)
-        {
-            this.tu=this.tu*a.mau;
-            this.mau=this.mau*a.tu;
-        }
-        public void toigianps()
-        {
-            int i= UCLN(this.tu,this.mau);
-           this.tu=this.tu/i;
-            this.mau=this.mau/i;
-           
-        }
-        
-        
-        
-        public void show()
-        {
-            System.out.println(getTu()+"/"+getMau());
-        }
-        
-        
-        
+
     }
-    
-        
-                    
-    
+
+    public static phanso nhan2phanso(phanso a, phanso b) {
+        int tu = a.tu * b.tu;
+        int mau = a.mau * b.mau;
+        phanso c = new phanso(tu, mau);
+        return c;
+    }
+
+    public static phanso cong2phanso(phanso a, phanso b) {
+        int tu = a.tu * b.mau + a.mau * b.tu;
+        int mau = a.mau * b.mau;
+        phanso c = new phanso(tu, mau);
+        return c;
+    }
+
+    public static phanso tru2phanso(phanso a, phanso b) {
+        int tu = a.tu * b.mau - a.mau * b.tu;
+        int mau = a.mau * b.mau;
+        phanso c = new phanso(tu, mau);
+        return c;
+    }
+
+    public static phanso chia2phanso(phanso a, phanso b) {
+        int tu = a.tu * b.mau;
+        int mau = a.mau * b.tu;
+        phanso c = new phanso(tu, mau);
+        return c;
+    }
+
 }
